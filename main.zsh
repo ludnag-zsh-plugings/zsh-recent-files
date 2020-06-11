c413f06c4f45a7a09ae7b12d71e99784c5964e2d
Ludvik <ludviknagelhus@gmail.com>
2020-06-12 00:28:42 +0200

    Refactor file with function which adds paths to file

--- main.zsh
+++ main.zsh
@@ -1,5 +1,5 @@
 export ZSH_RECENTS=$ZSH_HOME/my_plugins/recents
 
-source $ZSH_RECENTS/recents.zsh
+source $ZSH_RECENTS/save_path_to_recents.zsh
 source $ZSH_RECENTS/command_wrappers.zsh
 source $ZSH_RECENTS/keybindings.zsh
