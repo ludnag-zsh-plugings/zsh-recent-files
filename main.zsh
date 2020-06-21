eb023c0f99e27872e87bf2dd83e2f83255128bcd
Ludvik <ludviknagelhus@gmail.com>
2020-06-21 21:46:07 +0200

    Add explaining comment to main.zsh

--- main.zsh
+++ main.zsh
@@ -1,4 +1,5 @@
-export ZSH_RECENTS=$ZSH_HOME/my_plugins/recents
+# Set ZSH_RECENTS to the path of your installation
+export ZSH_RECENTS=$ZSH_HOME/my_plugins/zsh_recents
 
 source $ZSH_RECENTS/save_path_to_recents.zsh
 source $ZSH_RECENTS/command_wrappers.zsh
