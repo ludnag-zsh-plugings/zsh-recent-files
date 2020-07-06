84bfb034bcfb4a1907df913932b021873eecd2cf
Ludvik <ludviknagelhus@gmail.com>
2020-07-06 15:20:23 +0200

    Rename entry file

--- main.plugin.zsh
+++ main.plugin.zsh
@@ -1,5 +1,5 @@
 # Set ZSH_RECENTS to the path of your installation
-export ZSH_RECENTS=$ZSH_HOME/my_plugins/zsh_recents
+export ZSH_RECENTS=$ZSH_HOME/plugins/my_plugins/zsh_recents
 
 source $ZSH_RECENTS/save_path_to_recents.zsh
 source $ZSH_RECENTS/command_wrappers.zsh
